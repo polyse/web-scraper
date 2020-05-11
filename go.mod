9b9c1ab700497ec64b887ba9621b3eaea50d22ca
Кирилл Кудинов <=>
2020-05-11 15:27:44 +0300

    fix merge conflict

--- go.mod
+++ go.mod
@@ -7,14 +7,9 @@ require (
 	github.com/bobesa/go-domain-util v0.0.0-20190911083921-4033b5f7dd89
 	github.com/caarlos0/env/v6 v6.2.2
 	github.com/gocolly/colly/v2 v2.0.1
-	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/google/wire v0.4.0
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/mauidude/go-readability v0.0.0-20141216012317-2f30b1a346f1
 	github.com/rs/zerolog v1.18.0
 	go.zoe.im/surferua v0.0.1
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
-	golang.org/x/net v0.0.0-20200505041828-1ed23360d12c // indirect
-	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ internal/extractor/extractor_test.go
@@ -0,0 +1,26 @@
+package extractor
+
+import (
+	"testing"
+)
+
+func TestExtractor_ExtractContentFromHTML(t *testing.T) {
+	htmlString := "<html><head><title>Title</title></head><body><div><p>short content</p>" +
+		"<p>More content.</p>" +
+		"<p>Text with link in it: <a href=\"http://www.site.ru/\" class=\"link\">some link</a></p></div>" +
+		"<div id=\"banner\"></div><script>if (true) {}</script></div>" +
+		"<div>And another one content block.</div>" +
+		"<div><b>Some bold text.</b></div>" +
+		"<img src=\"image.jpg\" class=\"image\">" +
+		"<style>font-style:normal</style></body></html>"
+	expected := " And another one content block. Some bold text. short content More content. " +
+		"Text with link in it: some link     "
+	actual, err := ExtractContentFromHTML(htmlString)
+	if err != nil {
+		t.Errorf("\nunexpected error: %v", err)
+	}
+	actual = Clean(actual)
+	if actual != expected {
+		t.Errorf("\nactual:   %v \nexpected: %v", actual, expected)
+	}
+}
